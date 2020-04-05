67664f7b8c69a73b05654da1ffcb210fd37eb414
KaterynaKostrubova <katerinakostrubova@gmail.com>
2020-04-06 02:10:05 +0300

    add color

--- app.py
+++ app.py
@@ -75,7 +75,6 @@ class Equation:
 
             if degree == 1:
                 degree = ''
-                # tpl_v = '{}{}x{}'
 
             if get_absolute(int(coef)) == 1:
                 coef = ''
@@ -140,19 +139,21 @@ class Equation:
             return self._solve_simple(b, c)
         discriminant = b ** 2 - 4 * a * c
         if self.more:
-            print("Discriminant:", discriminant)
+            print("\033[1;35;40mDiscriminant:\033[1;33;40m", discriminant, '')
         if discriminant > 0:
             x1 = round((-b + sqrt(discriminant)) / (2 * a), 2)
             x2 = round((-b - sqrt(discriminant)) / (2 * a), 2)
-            print("Discriminant is strictly positive, the two solutions are:\n{}\n{}".format(x1, x2))
+            print("\033[1;36;40mDiscriminant is strictly positive, "
+                  "the two solutions are:\033[1;32;40m\n{}\n{}\033[0m".format(x1, x2))
 
         elif discriminant < 0:
             x1, x2 = self.find_complex_solutions(a, b, discriminant)
-            print("Discriminant is strictly negative, the two complex solutions are:\n{}\n{}".format(x1, x2))
+            print("\033[1;36;40mDiscriminant is strictly negative, the two "
+                  "complex solutions are:\033[1;32;40m\n{}\n{}\033[0m".format(x1, x2))
         else:
             x1 = round((-b + sqrt(discriminant)) / (2 * a), 2) or 0
             x2 = None
-            print("Discriminant is equal to zero, the solution is:\n{}".format(x1))
+            print("\033[1;36;40mDiscriminant is equal to zero, the solution is:\033[1;32;40m\n{}\033[0m".format(x1))
 
         return x1, x2
 
@@ -161,34 +162,34 @@ class Equation:
         reduced = self.reduce({FREE_COEFFICIENT_POWER: c, 1: b}, simplified=True)
 
         if b == 0 and c:
-            print('Unsolvable equation: {}'.format(reduced))
+            print('\033[1;31;40mUnsolvable equation:\033[1;33;40m {}\033[0m'.format(reduced))
             return result, None
 
         if b == 0:
-            print('All real numbers are solutions!')
+            print('\033[1;32;40mAll real numbers are solutions!\033[0m')
             return result, None
 
         result = round(-c / b, 2)
 
         # print('Simplified: {}'.format(reduced))
-        print('Solution: {}'.format(result))
+        print('\033[1;32;40mSolution: {}\033[0m'.format(result))
         return result, None
 
     def run(self):
         try:
             self.coefficients = self._parse()
         except NegativePower:
-            print('Oops.. invalid power, I can\'t solve this equation. Try another!')
+            print('\033[1;31;40mOops.. invalid power, I can\'t solve this equation. Try another!\033[0m')
             return
         except Exception as err:
             if self.debug:
                 print('PARSING_ERROR:', err)
-            print('Oops, that was no valid equation... Try another!')
+            print('\033[1;31;40mOops, that was no valid equation... Try another!\033[0m')
             return
-        print('Reduced form:', self.reduce())
-        print('Polynomial degree:', max(self.power, 0))
+        print('\033[1;34;40mReduced form:\033[1;33;40m', self.reduce())
+        print('\033[1;34;40mPolynomial degree:\033[1;33;40m', max(self.power, 0), '\033[0m')
         if self.power > 2:
-            print('The polynomial degree is strictly greater than 2, I can\'t solve.')
+            print('\033[1;31;40mThe polynomial degree is strictly greater than 2, I can\'t solve.\033[0m')
         else:
             return self.solve()
 
