6d9e032b2d7c70a8ed84f160c061328823466d82
KaterynaKostrubova <katerinakostrubova@gmail.com>
2020-04-07 17:41:06 +0300

    fix

--- app.py
+++ app.py
@@ -31,18 +31,13 @@ class Equation:
         self.debug = debug
         self.more = more
         self.coefficients = {}
-        # self.coefficients = self._parse()
 
-    @property
-    def power(self) -> int:
-        return max(list(self.coefficients.keys()))
-
-    def reduce(self, degrees: dict = None, simplified: bool = False) -> str:
+    def reduce(self, degrees: dict = None):
         tpl = []
-
         coefs = degrees or self.coefficients
+        coefs[-1] = coefs.get(-1, 0) + coefs.get(0, 0)
         degrees = list(coefs.keys())
-
+        max_degree = 0
         powers = {
             '0': u'\u2070',
             '1': u'\u00B9',
@@ -57,45 +52,47 @@ class Equation:
         }
 
         degrees.sort(reverse=True)
-        #
-        # if FREE_COEFFICIENT_POWER in degrees:
-        #     degrees = degrees[1:] + [degrees[0]]
 
         for i, degree in enumerate(degrees):
             tpl_v = '{}{}x{}'
             coef = coefs[degree]
+            free = 0
 
             if degree == FREE_COEFFICIENT_POWER:
+                free = 1
                 degree = ''
                 tpl_v = '{}{}{}'
-
-            if simplified and degree == 0:
-                degree = ''
-                tpl_v = '{}{}{}'
+            elif int(coef) == 1:
+                coef = ''
 
             if degree == 1:
                 degree = ''
 
-            if get_absolute(int(coef)) == 1:
-                coef = ''
-
             if coef != '' and coef < 0:
-                sign = '-' if i == 0 else '- '
+                sign = '-' if tpl == [] else '- '
                 coef = -coef
+                if get_absolute(int(coef)) == 1 and not free:
+                    coef = ''
             else:
-                sign = '' if i == 0 else '+ '
+                sign = '' if tpl == [] else '+ '
 
-            if coef != 0:
+            if coef != 0 and degree != 0:
                 if str(degree) in powers:
-                    v = tpl_v.format(sign, coef, powers[str(degree)])
+                    dgr = powers[str(degree)]
+                    v = tpl_v.format(sign, coef, dgr)
                     tpl.append(v)
                 else:
-                    d2 = ''
+                    dgr = ''
                     for index, char in enumerate(str(degree)):
-                        d2 = d2+powers[char]
-                    v = tpl_v.format(sign, coef, d2)
+                        dgr = dgr+powers[char]
+                    v = tpl_v.format(sign, coef, dgr)
                     tpl.append(v)
-
+                if free:
+                    max_degree = max_degree
+                elif degree == '':
+                    max_degree = 1 if max_degree < 1 else max_degree
+                elif max_degree < degree:
+                    max_degree = degree
         if tpl:
             tpl += ['= 0']
         else:
@@ -110,8 +107,7 @@ class Equation:
 
         for k, v in repl.items():
             res = res.replace(k, v)
-
-        return res
+        return res, max_degree
 
     def _parse(self) -> dict:
         return Parser(self._data, self.debug).run()
@@ -130,7 +126,7 @@ class Equation:
     def solve(self):
         a = self.coefficients.get(2, 0)
         b = self.coefficients.get(1, 0)
-        c = self.coefficients.get(-1, 0) + self.coefficients.get(0, 0)
+        c = self.coefficients.get(-1, 0)
         res = self._solve(a, b, c)
         return res
 
@@ -139,59 +135,70 @@ class Equation:
             return self._solve_simple(b, c)
         discriminant = b ** 2 - 4 * a * c
         if self.more:
-            print("\033[1;35;40mDiscriminant:\033[1;33;40m", discriminant, '')
+            print("\033[1;35mDiscriminant:\033[1;33m", discriminant, '')
         if discriminant > 0:
             x1 = round((-b + sqrt(discriminant)) / (2 * a), 2)
             x2 = round((-b - sqrt(discriminant)) / (2 * a), 2)
-            print("\033[1;36;40mDiscriminant is strictly positive, "
-                  "the two solutions are:\033[1;32;40m\n{}\n{}\033[0m".format(x1, x2))
+            print("\033[1;36mDiscriminant is strictly positive, "
+                  "the two solutions are:\033[1;32m\n{}\n{}\033[0m".format(x1, x2))
 
         elif discriminant < 0:
             x1, x2 = self.find_complex_solutions(a, b, discriminant)
-            print("\033[1;36;40mDiscriminant is strictly negative, the two "
-                  "complex solutions are:\033[1;32;40m\n{}\n{}\033[0m".format(x1, x2))
+            print("\033[1;36mDiscriminant is strictly negative, the two "
+                  "complex solutions are:\033[1;32m\n{}\n{}\033[0m".format(x1, x2))
         else:
             x1 = round((-b + sqrt(discriminant)) / (2 * a), 2) or 0
             x2 = None
-            print("\033[1;36;40mDiscriminant is equal to zero, the solution is:\033[1;32;40m\n{}\033[0m".format(x1))
+            print("\033[1;36mDiscriminant is equal to zero, the solution is:\033[1;32m\n{}\033[0m".format(x1))
 
         return x1, x2
 
     def _solve_simple(self, b, c):
         result = None
-        reduced = self.reduce({FREE_COEFFICIENT_POWER: c, 1: b}, simplified=True)
-
+        reduced, power = self.reduce({FREE_COEFFICIENT_POWER: c, 1: b})
         if b == 0 and c:
-            print('\033[1;31;40mUnsolvable equation:\033[1;33;40m {}\033[0m'.format(reduced))
+            print('\033[1;31mUnsolvable equation:\033[1;33m {}\033[0m'.format(reduced))
             return result, None
 
         if b == 0:
-            print('\033[1;32;40mAll real numbers are solutions!\033[0m')
+            print('\033[1;32mAll real numbers are solutions!\033[0m')
             return result, None
 
-        result = round(-c / b, 2)
-
-        # print('Simplified: {}'.format(reduced))
-        print('\033[1;32;40mSolution: {}\033[0m'.format(result))
+        result = round(-c / b, 2) or 0
+        print('\033[1;32mSolution: {}\033[0m'.format(result))
         return result, None
 
     def run(self):
         try:
             self.coefficients = self._parse()
         except NegativePower:
-            print('\033[1;31;40mOops.. invalid power, I can\'t solve this equation. Try another!\033[0m')
+            print('\033[1;31mOops.. invalid power, I can\'t solve this equation. Try another!\033[0m')
             return
         except Exception as err:
             if self.debug:
                 print('PARSING_ERROR:', err)
-            print('\033[1;31;40mOops, that was no valid equation... Try another!\033[0m')
+            print('\033[1;31mOops, that was no valid equation... Try another!\033[0m')
             return
-        print('\033[1;34;40mReduced form:\033[1;33;40m', self.reduce())
-        print('\033[1;34;40mPolynomial degree:\033[1;33;40m', max(self.power, 0), '\033[0m')
-        if self.power > 2:
-            print('\033[1;31;40mThe polynomial degree is strictly greater than 2, I can\'t solve.\033[0m')
-        else:
-            return self.solve()
+
+        try:
+            reduce_form, max_power = self.reduce()
+
+            print('\033[1;34mReduced form:\033[1;33m', reduce_form, '\033[0m')
+            print('\033[1;34mPolynomial degree:\033[1;33m', max_power, '\033[0m')
+
+            if max_power > 2:
+                print('\033[1;31mThe polynomial degree is strictly greater than 2, I can\'t solve.\033[0m')
+                return (None, None), reduce_form, max_power
+            else:
+                return self.solve(), reduce_form, max_power
+
+        except Exception as err:
+            print('\033[1;31mOops, something go wrong... Try another equation!\033[0m')
+            if self.debug:
+                print('SOLVING_ERROR:', err)
+
+
+
 
 # if __name__ == '__main__':
 #     Equation("2*x^0-1=-3.2").run()
